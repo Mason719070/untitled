84c8c91383bb22ee6b0ff92e2f575e1588bf3641
Mason719070 <122560385+Mason719070@users.noreply.github.com>
2023-01-20 12:52:57 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ input.onButtonPressed(Button.AB, function () {
     music.playMelody("B C5 E D E A G F ", 120)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    basic.showString("will you be my valentine")
 })
 input.onGesture(Gesture.Shake, function () {
     music.playSoundEffect(music.createSoundEffect(
